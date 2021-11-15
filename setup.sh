aa4dd848617e00d80db512dc16c97b8d58d04682
Asiegbu-Cynthia-Chisom <asiegbucynthia.cc@gmail.com>
2021-11-15 10:55:19 +0100

    update setup file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-export DATABASE_URL=postgres://zycfwhsmtlxzim:825d59a75a5b9186cdce88556d1efc216afac0f19a9efc05c7f28b6f4a5a6bdc@ec2-18-206-108-36.compute-1.amazonaws.com:5432/d57370du7b30d
-export DATABASE_URL=postgresql://postgres:root@localhost:5000/capstone
+export DATABASE_URL
+export DATABASE_URL
 
 export FLASK_APP=app.py;
 export AUTHO_DOMAIN='capstone-projekt.us.auth0.com'
