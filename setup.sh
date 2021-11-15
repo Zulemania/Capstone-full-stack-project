4d3047eadcc3f394c337aa02146e6e8bdba7ea26
Asiegbu-Cynthia-Chisom <asiegbucynthia.cc@gmail.com>
2021-11-15 10:47:02 +0100

    update setup file

---
+++ setup.sh
@@ -0,0 +1,12 @@
+export DATABASE_URL=postgres://zycfwhsmtlxzim:825d59a75a5b9186cdce88556d1efc216afac0f19a9efc05c7f28b6f4a5a6bdc@ec2-18-206-108-36.compute-1.amazonaws.com:5432/d57370du7b30d
+export DATABASE_URL=postgresql://postgres:root@localhost:5000/capstone
+
+export FLASK_APP=app.py;
+export AUTHO_DOMAIN='capstone-projekt.us.auth0.com'
+export ALGORITHMS=['RS256']
+export API_AUDIENCE='magic'
+export CLIENT_ID='06lGES4xRP9Z7cZu6Uyj52iyK7bCq5Pw'
+
+export CASTING_ASSISTANT
+export CASTING_DIRECTOR
+export EXECUTIVE_PRODUCER
