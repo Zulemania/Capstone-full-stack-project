2ebdcb9983c79d6270c1ba65c1720b397b203745
Asiegbu-Cynthia-Chisom <asiegbucynthia.cc@gmail.com>
2021-11-19 15:33:33 +0100

    Implement error handling' quit() q q:!

--- app.py
+++ app.py
@@ -187,21 +187,48 @@ def create_app(test_config=None):
             'total_movies': len(Movie.query.all())
         }), 200
 
-    
+    @app.errorhandler(400)
+    def bad_request_error(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "bad request"
+        }), 400
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
+
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        return jsonify({
+            "success": False,
+            "error": 405,
+            "message": "method not allowed"
+        }), 405
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({
+            "success": False,
+            "error": 500,
+            "message": "internal server error"
+        }), 500
 
     
-
     
-
-
-
-    
-
-
-
-        
-
-
         
         
 
